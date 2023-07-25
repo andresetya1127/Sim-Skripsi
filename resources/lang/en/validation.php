8631729cf79923e466ff206757699715a4b8c490
andresetya <andresetya11277@gmail.com>
2023-07-26 05:04:50 +0800

    tambah crud tema

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -125,7 +125,7 @@ return [
     'starts_with' => 'The :attribute must start with one of the following: :values.',
     'string' => 'The :attribute must be a string.',
     'timezone' => 'The :attribute must be a valid timezone.',
-    'unique' => ':attribute tidak boleh sama.',
+    'unique' => ':attribute sudah ada.',
     'uploaded' => 'The :attribute failed to upload.',
     'url' => 'The :attribute must be a valid URL.',
     'uuid' => 'The :attribute must be a valid UUID.',
